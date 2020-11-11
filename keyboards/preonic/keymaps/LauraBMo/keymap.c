f4b3f6d0f3d6a2cf1af38262043ba00bb8ca80cd
Laura Brustenga i Moncusí <brust@math.ku.dk>
2020-11-11 22:42:39 +0100

    add Alt_Tab key

--- keyboards/preonic/keymaps/LauraBMo/keymap.c
+++ keyboards/preonic/keymaps/LauraBMo/keymap.c
@@ -19,6 +19,10 @@
 #include "keymap_danish.h"
 #include "keymap_spanish.h"
 
+/* For the Alt_Tab key */
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
 enum preonic_layers {
   _QWERTY,
   _QWERTY_ESP,
@@ -32,7 +36,8 @@ enum preonic_keycodes {
   QWERTY_ESP,
   LOWER,
   RAISE,
-  BACKLIT
+  BACKLIT,
+  ALT_TAB
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  ALT_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
@@ -196,6 +201,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
+          case ALT_TAB:
+          if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+              is_alt_tab_active = true;
+              register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+          } else {
+            unregister_code(KC_TAB);
+          }
+          return false;
+          break;
       }
     return true;
 };
@@ -270,6 +288,14 @@ void matrix_scan_user(void) {
         }
     }
 #endif
+/* For the Alt_Tab key */
+/* The very important timer. */
+    if (is_alt_tab_active) {
+      if (timer_elapsed(alt_tab_timer) > 1000) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
+      }
+    }
 }
 
 bool music_mask_user(uint16_t keycode) {
