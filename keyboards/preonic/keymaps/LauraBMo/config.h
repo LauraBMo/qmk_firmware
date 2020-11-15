f8c4f243b23657f8aa3a7178942dbe448364e656
Laura Brustenga i Moncusí <brust@math.ku.dk>
2020-11-15 02:35:38 +0100

    add Space Cadet keys
    Mod Tab does not work for shifted keys...

--- keyboards/preonic/keymaps/LauraBMo/config.h
+++ keyboards/preonic/keymaps/LauraBMo/config.h
@@ -38,3 +38,7 @@
 
 /* Input mode for unicode chars: UC_MAC, UC_LNX or UC_WINC */
 #define UNICODE_SELECTED_MODES UC_LNX
+
+/* Space Cadet Shift */
+/* https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_space_cadet */
+#define LSPO_KEYS KC_LGUI, KC_LSFT, KC_SCLN
--- keyboards/preonic/keymaps/LauraBMo/rules.mk
+++ keyboards/preonic/keymaps/LauraBMo/rules.mk
@@ -5,3 +5,6 @@ UNICODEMAP_ENABLE = yes
 
 # audio is fun
 AUDIO_ENABLE = yes
+
+# Space Cadet's functionality can conflict with the default Command functionality
+COMMAND_ENABLE = no
